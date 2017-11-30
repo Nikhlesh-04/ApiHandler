a65e2204768cc66d2444600f38d7b0d1c3a2a1ac
nikky bagdiya <Nikhlesh@nikkys-macbook-pro.local>
2017-11-30 18:09:12 +0530

    app date uiimageview loader function.

--- Pinboard/ApiHandler+UIImageView.swift
+++ Pinboard/ApiHandler+UIImageView.swift
@@ -10,11 +10,13 @@ import UIKit
 
 extension UIImageView {
     
-    @discardableResult public func loadImageWithUrl(url: URL ,with placeHolderImage:UIImage) -> URLSessionDataTask {
+    @discardableResult public func loadImageWithUrl(url: URL ,with placeHolderImage:UIImage?=nil) -> URLSessionDataTask {
         
         let imageURL = url
         
-        image = placeHolderImage
+        if (placeHolderImage != nil) {
+            image = placeHolderImage
+        }
         
         // retrieves image if already available in cache
         if let imageFromCache = Constants.kImageCache.object(forKey: url as AnyObject) as? UIImage {
@@ -46,37 +48,4 @@ extension UIImageView {
         return tasks
     }
     
-    @discardableResult public func loadImageWithUrl(url: URL) -> URLSessionDataTask {
-        
-        let imageURL = url
-        
-        // retrieves image if already available in cache
-        if let imageFromCache = Constants.kImageCache.object(forKey: url as AnyObject) as? UIImage {
-            
-            self.image = imageFromCache
-            return URLSessionDataTask()
-        }
-        
-        // image does not available in cache.. so retrieving it from url...
-        let tasks = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                print(error as Any)
-                return
-            }
-            
-            DispatchQueue.main.async(execute: {
-                
-                if let unwrappedData = data, let imageToCache = UIImage(data: unwrappedData) {
-                    
-                    if imageURL == url {
-                        self.image = imageToCache
-                    }
-                    
-                    Constants.kImageCache.setObject(imageToCache, forKey: url as AnyObject)
-                }
-            })
-        }
-        tasks.resume()
-        return tasks
-    }
 }
